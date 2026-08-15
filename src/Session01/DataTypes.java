f09c472b77f230caad0ff5a925f6df25541e4fa6
ENG Farouk <ENG Farouk@ENG-FAROUK>
2026-08-15 17:30:04 +0300

    Initial commit

---
+++ src/Session01/DataTypes.java
@@ -0,0 +1,41 @@
+package Session01;
+
+public class DataTypes {
+    static void main() {
+        // DataTypes
+//        int a ; // Variable Declaration
+//        int a = 10, b = 10; // Variable Declaration + Initialization
+
+//        char a = 'a';
+//        String name = "Farouk";
+
+//        byte a = 127;
+//        System.out.println("The byte Value is " + a);
+//
+//        short sh = 32000;
+//        System.out.println("The short Value is " + sh);
+//
+//        int b = 127;
+//        System.out.println("The int Value is " + b);
+//
+//        long l = 127525115L;
+//        System.out.println("The long Value is " + l);
+//
+//        float f = 12821F;
+//        System.out.println("The float Value is " + f);
+//
+//        double d = 127.215510D;
+//        System.out.println("The double Value is " + d);
+//
+//        boolean bool = true;
+//        System.out.println("The boolean Value is " + bool);
+//
+//        char grade = 'A';
+//        System.out.println("The char Value is " + grade);
+
+//        int a = 1552;
+        long b = 151551511111111L;
+        int a = (int) b;
+        System.out.println(a);
+    }
+}
