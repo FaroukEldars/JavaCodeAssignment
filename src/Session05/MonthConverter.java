5c16e7021d62946913fc20dfea7d9e3928c20b8f
ENG Farouk <ENG Farouk@ENG-FAROUK>
2026-09-09 01:00:48 +0300

    Session05

---
+++ src/Session05/MonthConverter.java
@@ -0,0 +1,9 @@
+package Session05;
+
+public class MonthConverter {
+    public static void main(String[] args) {
+        System.out.println(Month.getMonth("Feb"));
+        System.out.println(Month.getMonth(1));
+    }
+
+}
